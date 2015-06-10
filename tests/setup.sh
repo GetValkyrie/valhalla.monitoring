80423919328cc9d31c15db1aeee5b726e62d248a
Christopher Gervais <chris@ergonlogic.com>
2015-06-10 16:40:58 +0000

    Initial commit.

---
+++ tests/setup.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+curl https://raw.githubusercontent.com/GetValkyrie/ansible-bootstrap/master/install-ansible.sh | /bin/sh
+ansible-galaxy install --ignore-errors geerlingguy.mysql
+ansible-galaxy install --ignore-errors http://github.com/getvalkyrie/valhalla.common,,valhalla.common
+ansible-galaxy install --ignore-errors http://github.com/getvalkyrie/valhalla.web,,valhalla.web
+ansible-galaxy install --ignore-errors http://github.com/getvalkyrie/ansible-role-graphite,,getvalkyrie.graphite
+ansible-galaxy install --ignore-errors http://github.com/getvalkyrie/ansible-role-shinken,,getvalkyrie.shinken
+
+target=/etc/ansible/roles/valhalla.web
+if [ ! -e $target ]; then
+  mkdir -p /etc/ansible/roles/
+  ln -s /vagrant/ $target
+fi
